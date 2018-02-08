d4893e4badd40a9e4925405d207c95c074538f6f
Darek Strojecki <dstroj@wp.pl>
2018-02-08 12:54:27 +0100

    Add class Plots.

---
+++ src/main/java/pl/strojecki/models/Plots.java
@@ -0,0 +1,12 @@
+package pl.strojecki.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Plots {
+    private List<Plot> plots = new ArrayList<>();
+
+    public void update(Plot plot){
+
+    }
+}
